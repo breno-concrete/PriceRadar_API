b89a70ada28ea9d391dbcb7a331275ce8d616b9c
breno-concrete <brenocount@gmail.com>
2026-06-05 12:50:49 -0300

    feat: rename application properties file and enhance test coverage for service beans (CI/CD)

--- src/test/java/com/breno/PriceRadar_API/PriceRadarApiApplicationTests.java
+++ src/test/java/com/breno/PriceRadar_API/PriceRadarApiApplicationTests.java
@@ -1,13 +1,38 @@
 package com.breno.PriceRadar_API;
 
+import com.breno.PriceRadar_API.services.AlertService;
+import com.breno.PriceRadar_API.services.SnapshotService;
+import com.breno.PriceRadar_API.services.TrackedItemService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class PriceRadarApiApplicationTests {
+
+	@Autowired
+	private TrackedItemService trackedItemService;
+
+	@Autowired
+	private SnapshotService snapshotService;
+
+	@Autowired
+	private AlertService alertService;
 
 	@Test
 	void contextLoads() {
+		// Verifica que o contexto Spring carrega sem erros
+	}
+
+	@Test
+	void serviceBeansAreLoaded() {
+		assertNotNull(trackedItemService, "TrackedItemService should be loaded");
+		assertNotNull(snapshotService, "SnapshotService should be loaded");
+		assertNotNull(alertService, "AlertService should be loaded");
 	}
 
 }
